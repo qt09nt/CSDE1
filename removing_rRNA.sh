23b532e9a130ca41c5c11c6ba0b7930955ae1619
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-09-22 16:45:31 -0600

    Update removing_rRNA.sh

--- removing_rRNA.sh
+++ removing_rRNA.sh
@@ -1,3 +1,6 @@
 #this script is to remove rRNA contamination from the CSDE1 RIP sequencing
 #using a software called RiboDetector
 https://github.com/hzi-bifo/RiboDetector
+
+#login to ARC
+cd /work/yang_lab/1_Software
