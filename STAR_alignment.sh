70daeeb7f142ce6fd09d3cf5556831b815b9c221
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-09-27 16:22:55 -0600

    Update STAR_alignment.sh

--- STAR_alignment.sh
+++ STAR_alignment.sh
@@ -1,6 +1,13 @@
 #alignment of mouse genome reference file and gtf annotation file where rRNA and noncoding RNA removed
 #submit as a batch job 
 
+#!/bin/bash
+#SBATCH --partition=bigmem
+#SBATCH --time=6:0:0
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH -cpus-per-task=8
+#SBATCH --mem=8gb
 
 
 module load biobuilds/2017.11
