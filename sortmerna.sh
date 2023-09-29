f7c97d15759903a7f958dd8a1f4cfca939ba9640
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-09-29 15:33:45 -0600

    Create sortmerna.sh

---
+++ sortmerna.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#<------------------------Request for Resources----------------------->
+#SBATCH -J sortmerna
+#SBATCH --partition=bigmem,cpu2017-bf05
+#SBATCH --time=6:0:0
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=0
+
+module load biobuilds/2017.11
+
+REF_SSU=/work/yang_lab/queenie/SILVA_138.1_SSURef_NR99_tax_silva.fasta
+REF_LSU=/work/yang_lab/queenie/SILVA_138.1_LSURef_NR99_tax_silva.fasta
+input=/work/yang_lab/csde1_RIP/*
+
+# Run
+i=0
+
+for FILE in $input*fastq
+do echo "trials starting for"
+echo $i
+echo "Accessing this file"
+echo $FILE
+
+#clear the contents in the kvdb directory prior to each new run
+rm -rf $HOME/sortmerna/run/kvdb/
+
+##### run sortmerna 
+sortmerna --ref $REF_SSU \
+--ref $REF_LSU \
+--reads /work/yang_lab/queenie/csde1_RIP/Li35179_S1_R1_001.fastq 
