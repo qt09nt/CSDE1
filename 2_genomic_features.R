f3e9438a0744b453add27bbb5ba22ba9d4b3a2f7
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-03 16:34:30 -0600

    Update 2_genomic_features.R

--- 2_genomic_features.R
+++ 2_genomic_features.R
@@ -64,3 +64,95 @@ seqlevels(txdb) <- seqlevels0(txdb)
 seqlevels(txdb) <- "chr15"
 seqlevels(txdb)
 
+seqlevels(txdb)
+
+#Retrieving data using the select method
+
+# The TxDb objects inherit from AnnotationDb objects (just as
+#the ChipDb and OrgDb objects do). One of the implications of this 
+#relationship is that these object ought to be used in similar ways
+#to each other. Therefore we have written supporting columns, keytypes,
+#keys and select methods for TxDb objects.
+#
+# These methods can be a useful way of extracting data from a TxDb 
+#object. And they are used in the same way that they would be used
+#to extract information about a ChipDb or an OrgDb object. Here is 
+#a simple example of how to find the UCSC transcript names that match 
+#with a set of gene IDs.
+
+keys <- c("100033416", "100033417", "100033420")
+columns(txdb)
+# [1] "CDSCHROM"   "CDSEND"     "CDSID"      "CDSNAME"    "CDSSTART"  
+# [6] "CDSSTRAND"  "EXONCHROM"  "EXONEND"    "EXONID"     "EXONNAME"  
+# [11] "EXONRANK"   "EXONSTART"  "EXONSTRAND" "GENEID"     "TXCHROM"   
+# [16] "TXEND"      "TXID"       "TXNAME"     "TXSTART"    "TXSTRAND"  
+# [21] "TXTYPE"   
+
+keytypes(txdb)
+# [1] "CDSID"    "CDSNAME"  "EXONID"   "EXONNAME" "GENEID"   "TXID"    
+# [7] "TXNAME" 
+
+select(txdb, keys = keys, columns = "TXNAME", keytype="GENEID")
+#     GENEID     TXNAME
+# 1 100033416 uc001yxl.4
+# 2 100033417 uc001yxo.3
+# 3 100033420 uc001yxr.3
+
+columns (txdb)
+# [1] "CDSCHROM"   "CDSEND"     "CDSID"      "CDSNAME"    "CDSSTART"  
+# [6] "CDSSTRAND"  "EXONCHROM"  "EXONEND"    "EXONID"     "EXONNAME"  
+# [11] "EXONRANK"   "EXONSTART"  "EXONSTRAND" "GENEID"     "TXCHROM"   
+# [16] "TXEND"      "TXID"       "TXNAME"     "TXSTART"    "TXSTRAND"  
+# [21] "TXTYPE
+
+cols <- c("TXNAME", "TXSTRAND", "TXCHROM")
+select(txdb, keys=keys, columns=cols, keytype="GENEID")
+
+# Methods for returning GRanges objects
+# 
+# Retrieving data with select is useful, but sometimes it is more convenient to extract the result as GRanges objects. This is often the case when you are doing counting or specialized overlap operations downstream. For these use cases there is another family of methods available.
+# 
+# Perhaps the most common operations for a TxDb object is to retrieve the genomic coordinates or ranges for exons, transcripts or coding sequences. The functions transcripts, exons, and cds return the coordinate information as a GRanges object.
+# 
+# As an example, all transcripts present in a TxDb object can be obtained as follows:
+
+GR <- transcripts (txdb)
+GR[1:3]
+# GRanges object with 3 ranges and 2 metadata columns:
+#   seqnames            ranges strand |     tx_id
+# <Rle>         <IRanges>  <Rle> | <integer>
+#   [1]    chr15 20362688-20364420      + |     53552
+# [2]    chr15 20487997-20496811      + |     53553
+# [3]    chr15 20723929-20727150      + |     53554
+# tx_name
+# <character>
+#   [1]  uc001yte.1
+# [2]  uc001ytf.1
+# [3]  uc001ytj.3
+# -------
+#   seqinfo: 1 sequence from hg19 genome
+
+# The transcripts function returns a GRanges class 
+#object. You can learn a lot more about the manipulation
+#of these objects by reading the GenomicRanges
+#introductory vignette. The show method for a GRanges 
+#object will display the ranges, seqnames (a chromosome
+#or a contig), and strand on the left side and then 
+#present related metadata on the right side. At the 
+#bottom, the seqlengths display all the possible
+#seqnames along with the length of each sequence.
+
+# 
+# The strand function is used to obtain the strand information from the transcripts. The sum of the Lengths of the Rle object that strand returns is equal to the length of the GRanges object.
+
+tx_strand <- strand(GR)
+tx_strand
+
+sum(runLength(tx_strand))
+#3337
+
+length(GR)
+
+# In addition, the transcripts function can also be used to retrieve a subset of the transcripts available such as those on the
+#\(+\)-strand of chromosome 1.
+
