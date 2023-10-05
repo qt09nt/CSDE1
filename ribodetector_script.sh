f57631c4d47a7a3468cdd9876cb9fb768989c439
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-05 15:01:28 -0600

    Update ribodetector_script.sh

--- ribodetector_script.sh
+++ ribodetector_script.sh
@@ -2,9 +2,9 @@
 #<------------------------Request for Resources----------------------->
 #SBATCH -J ribodetector
 #SBATCH --mem=30G
-#SBATCH --time 01:00:00 
-#SBATCH -c 20 
-#SBATCH --partition=gpu-v100 
+#SBATCH --time 01:00:00
+#SBATCH -c 20
+#SBATCH --partition=gpu-v100
 #SBATCH --gres=gpu:1
 
 cd /work/yang_lab/1_Software/
@@ -15,15 +15,18 @@ input=/work/yang_lab/queenie/csde1_RIP/fastq/*
 i=0
 
 for FILE in $input*fastq
-do echo "sortmerna starting for"
+do echo "ribodetector starting for"
 echo $i
 echo "Accessing this file"
 echo $FILE
 
 ##### Run ribodetector GPU version
 apptainer run --nv -B /work/yang_lab ribodetector_image.sif ribodetector -t 20 -l 100 \
-          -i /work/yang_lab/queenie/csde1_RIP/fastq/${FILE} -m 12 -e norrna \
-          --chunk_size 256 -o /work/yang_lab/queenie/csde1_RIP/ribodetector/${FILE}.norrna.fastq
+          -i $FILE -m 12 -e norrna \
+          --chunk_size 256 -o "$FILE.norrna.fastq"
+
+#### move the output file to new folder
+mv *.norrna.fastq /work/yang_lab/queenie/csde1_RIP/ribodetector/
 
 "trial" $i "complete"
 ((i++))
