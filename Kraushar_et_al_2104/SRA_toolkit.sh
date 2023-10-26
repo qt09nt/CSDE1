8d39246bf42333fe6799b3e67febbd45c86dcab7
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-26 11:51:39 -0600

    Update SRA_toolkit.sh

--- Kraushar_et_al_2104/SRA_toolkit.sh
+++ Kraushar_et_al_2104/SRA_toolkit.sh
@@ -1,1 +1,7 @@
 https://github.com/ncbi/sra-tools/wiki/01.-Downloading-SRA-Toolkit
+
+#change directory to /work/yang_lab/1_Software/
+
+salloc --mem=10G --time 01:00:00 --partition=single
+
+https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.0.7/sratoolkit.3.0.7-centos_linux64.tar.gz
