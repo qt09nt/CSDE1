db8b3e8fa25e7bdc97cc3fb6589e75e922915c26
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-26 11:59:33 -0600

    Update SRA_toolkit.sh

--- Kraushar_et_al_2104/SRA_toolkit.sh
+++ Kraushar_et_al_2104/SRA_toolkit.sh
@@ -10,3 +10,8 @@ wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/3.0.7/sratoolkit.3.0.7-centos_li
 
 #extract toolkit
 tar -xzf sratoolkit.3.0.7-centos_linux64.tar.gz
+
+#get these samples
+GSM1229978: M_P0_WT_0; Mus musculus; RNA-Seq (SRR980305)
+SRX349397: GSM1229979: M_P0_WT_1; Mus musculus; RNA-Seq (SRR980306)
+SRX349398: GSM1229980: M_P0_WT_2; Mus musculus; RNA-Seq (SRR980307)
