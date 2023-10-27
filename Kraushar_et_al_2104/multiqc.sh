0d7314f908f0cc0a55c374f341517d93d452db52
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-27 10:09:38 -0600

    Update multiqc.sh

--- Kraushar_et_al_2104/multiqc.sh
+++ Kraushar_et_al_2104/multiqc.sh
@@ -4,17 +4,17 @@
 # Launch an interactive session:
 $ salloc --mem=10G --time 01:00:00 --partition=single
 
-Load bioconda
+#Load bioconda
 
 module load bioconda/conda3
 cd /work/yang_lab/1_Software
 
-Create new conda environment
+#Create new conda environment
 conda create --name multiqc python=3.11
 
 source activate multiqc
 
-To install MultiQC within the multiqc virtual environment
+#To install MultiQC within the multiqc virtual environment
 pip install multiqc
 
 #navigate to directory where the fastqc reports are located
@@ -23,10 +23,10 @@ cd /work/yang_lab/queenie/Celf2-KI-Polysome-seq/fastqc
 #run multiqc
 multiqc .
 
-To download the multiqc reports (this is one single command) open a home terminal on local machine: 
+#To download the multiqc reports (this is one single command) open a home terminal on local machine: 
 rsync -v queenie.tsang@arc-dtn.ucalgary.ca:/work/yang_lab/queenie/Celf2-KI-Polysome-seq/fastqc/multiqc_report.html .
 
-To download the multiqc data directory:
+#To download the multiqc data directory:
 Rsync -axv queenie.tsang@arc-dtn.ucalgary.ca:/work/yang_lab/queenie/Celf2-KI-Polysome-seq/fastqc/multiqc_data .
 
 
