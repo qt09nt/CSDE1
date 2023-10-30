7f8f0429a8de8eae7779e688a472567c00b0715f
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-30 15:09:51 -0600

    Update ribodetector.sh

--- Kraushar_et_al_2104/ribodetector.sh
+++ Kraushar_et_al_2104/ribodetector.sh
@@ -7,3 +7,8 @@
 #SBATCH --partition=gpu-v100
 #SBATCH --gres=gpu:1
 
+#print second column (fastq file 2) name
+cat samples.txt | while read line; do awk '{print $2}';  done
+
+#save first column of line in samples.txt into the variable $file1
+cat samples.txt | while read line; do file1=`awk '{print $1}'`; echo $file1;  done
