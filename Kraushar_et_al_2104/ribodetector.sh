dea40129883cf7117152f41e72cfbfedfbe6d893
Queenie Tsang <45493336+qt09nt@users.noreply.github.com>
2023-10-31 11:55:00 -0600

    Update ribodetector.sh

--- Kraushar_et_al_2104/ribodetector.sh
+++ Kraushar_et_al_2104/ribodetector.sh
@@ -1,3 +1,5 @@
+###NOTE THIS SCRIPT STILL HAS ERRORS WHEN RUN 
+
 #!/bin/bash
 #<------------------------Request for Resources----------------------->
 #SBATCH -J ribodetector
@@ -14,3 +16,26 @@ cat samples.txt | while read line; do awk '{print $2}';  done
 cat samples.txt | while read line; do file1=`awk '{print $1}'`; echo $file1;  done
 
 cat samples.txt | while read line; do file1=`awk '{print $2}'`; echo $file1;  done
+
+cd /work/yang_lab/1_Software/
+
+input=/work/yang_lab/queenie/krausher_2014/fastq/
+output=/work/yang_lab/queenie/krausher_2014/ribodetector/
+
+# Run
+i=0
+
+##### Run ribodetector GPU version
+        apptainer run --nv -B /work/yang_lab ribodetector_image.sif ribodetector -t 20 -l 100 \
+                -i "${input}${file1}" "${input}${file2}" \
+                -m 12 \
+                -e norrna \
+                --chunk_size 256 \
+                -o "${output}${file1}.norrna.fastq" "${output}${file2}.norrna.fastq"
+
+cd /work/yang_lab/1_Software/
+
+echo "trial" $i "complete"
+((i++))
+echo "-------------"
+done
